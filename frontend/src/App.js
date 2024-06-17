7489deff16f695dd160acbea1a86031c6d551f7f
anish0905 <anishanish0102@gmail.com>
2024-06-17 09:00:18 +0530

    update the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import './App.css';
-import Home from "./components/Home/Home";
+
 
 function App() {
   return (
   <>
         <Outlet />
-        <Home/>
+      
       
   </>
   );
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -3,17 +3,22 @@ import { CiPhone } from 'react-icons/ci';
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="flex items-center space-x-4 p-6 bg-white rounded-lg shadow-lg">
-        <div className="flex flex-col items-center space-y-2">
-          <div className="text-blue-500">
-            <CiPhone size={24} />
-          </div>
-          <span className="text-lg font-bold text-gray-700">Phone</span>
-          <span className="text-base text-gray-600">75222</span>
+    <div className="bg-gray-100 p-6">
+
+      <div className="flex items-center space-x-4">
+        <div className="text-blue-500">
+          <CiPhone size={24} />
         </div>
+        <div>
+          <p className="text-lg font-bold text-gray-700">Phone</p>
+          <p className="text-base text-gray-600">7260858715</p>
+        </div>
+        <div
+    class="inline-block h-[250px] min-h-[1em] w-0.5 self-stretch bg-red-100"></div>
+  <p class="ps-6">Text</p>
+</div>
       </div>
-    </div>
+  
   );
 }
 
